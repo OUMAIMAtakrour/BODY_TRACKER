a03f258990b42efe4e765dbb598c4d1f4828fd18
OUMAIMAtakrour <takrouroumaima@gmail.com>
2025-01-11 20:44:16 +0100

    Feat:data persisted successfully

--- app/(tabs)/register.tsx
+++ app/(tabs)/register.tsx
@@ -1,18 +1,95 @@
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, Alert } from "react-native";
 import { Picker } from "@react-native-picker/picker";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { ScrollView } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    Age: "",
+    Nationality: "",
+    Height: "",
+    Weight: "",
+  });
+
+  useEffect(() => {
+    loadSaveData();
+  }, []);
+  const loadSaveData = async () => {
+    try {
+      const userInfos = await AsyncStorage.getItem("formData");
+      if (userInfos) {
+        setFormData(JSON.parse(userInfos));
+      }
+    } catch (error) {
+      Alert.alert("failed to");
+    }
+  };
+  const savedInfos = async () => {
+    try {
+      await AsyncStorage.setItem("formData", JSON.stringify(formData));
+      Alert.alert("success", "data mchat");
+    } catch (error) {
+      Alert.alert("Error", "ma mchat");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.body}>
         <Text style={styles.title}>BODY TRACK</Text>
-        <TextInput style={styles.input} placeholder="Email" />
         <TextInput
+          style={styles.input}
+          placeholder="FullName"
+          value={formData.name}
+          onChangeText={(text) => setFormData({ ...formData, name: text })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Age"
+          value={formData.Age}
+          onChangeText={(text) => setFormData({ ...formData, Age: text })}
+          keyboardType="phone-pad"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Nationality"
+          value={formData.Nationality}
+          onChangeText={(text) =>
+            setFormData({ ...formData, Nationality: text })
+          }
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Height"
+          value={formData.Height}
+          onChangeText={(text) => setFormData({ ...formData, Height: text })}
+          keyboardType="phone-pad"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Weight"
+          value={formData.Weight}
+          onChangeText={(text) => setFormData({ ...formData, Weight: text })}
+          keyboardType="phone-pad"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={formData.email}
+          onChangeText={(text) => setFormData({ ...formData, email: text })}
+        />
+        {/* <TextInput
           style={styles.input}
           secureTextEntry={true}
           placeholder="Password"
-        />
+        /> */}
+        <Button
+          title="Submit"
+          style={styles.button}
+          onPress={savedInfos}
+        ></Button>
       </View>
     </View>
   );
@@ -52,5 +129,10 @@ const styles = StyleSheet.create({
   },
   picker: {
     marginVertical: 10,
+  },
+  button: {
+    color: "#0000ff",
+    borderRadius: 15,
+    margin: 20,
   },
 });
